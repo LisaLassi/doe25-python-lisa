e49a38d144694ce795542d0f17994db9bd245e7e
Lisan <lisa.lassi@live.se>
2025-09-22 15:01:05 +0200

    uppdaterad

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("Hello, World!")
+print("Hello, user")
 print("Vad heter du?")
 name = input()
 print("Hej" + " " + name + "!")
@@ -7,6 +7,6 @@ age = input()
 print ("Du är" + " " + age + " " + "år gammal!")
 print("Ange två tal:")
 num1 = input("Tal 1: ")
-num2 = input("Tal 2:")
+num2 = input("Tal 2: ")
 sum = int(num1) + int(num2) #Måste göra om num1 & num2 till en int från en string då det läses som en string och lägger ihop talen endast utan att addera dom.
 print("Summan av talen är:" + " " + str(sum)) #Måste här göra om int till en string för att kunna skriva ut.
