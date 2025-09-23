bcafe1eed8d5ff3f807157a5f0ea221cd28439d9
Lisan <lisa.lassi@live.se>
2025-09-23 12:46:49 +0200

    inför uppgift

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+'''
 print("Hello, user")
 print("Vad heter du?")
 name = input()
@@ -10,3 +11,24 @@ num1 = input("Tal 1: ")
 num2 = input("Tal 2: ")
 sum = int(num1) + int(num2) #Måste göra om num1 & num2 till en int från en string då det läses som en string och lägger ihop talen endast utan att addera dom.
 print("Summan av talen är:" + " " + str(sum)) #Måste här göra om int till en string för att kunna skriva ut.
+'''
+
+username = input("Hej, vad heter du?: ")
+age = input("Hur gammal är du?: ")
+
+if int(age) < 18:
+    print("Du är minderårig!")
+
+elif int(age) >= 18:
+    print ("Du är myndig!")
+
+'''
+elif int(age) != int:
+    print ("Du måste ange en siffra!")
+
+else:
+    print("Något blev fel!")
+'''
+
+for x in range (5):
+    print("Hej" + " " + username + "!")
