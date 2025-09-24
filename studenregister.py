102eeb0f8569bc477ae23d0ab54ebba9dc479b62
Lisan <lisa.lassi@live.se>
2025-09-24 18:12:39 +0200

    loop

--- studenregister.py
+++ studenregister.py
@@ -2,25 +2,24 @@ menu_is_running = True
 
 while menu_is_running:
     menu_choice = input('Gör ett menyval \n[1] LÄGG TILL STUDENT \n[2] LISTA ALLA STUDENTER \n[3] AVSLUTA \n Ditt val:')
-        
+
+    studenregister ={
+        'Förnamn' : {name},
+        'Efternamn' : {lastname},
+        'Ålder' : {age}
+        }
+
     if menu_choice == '1':
         print('Du valde att lägga till en student, ange förnamn & efternamn: ')
         name = input('Förnamn: ')
         lastname = input('Efternamn: ')
         age = input('Ålder: ')
 
-        studenregister ={
-        'Förnamn' : {name},
-        'Efternamn' : {lastname},
-        'Ålder' : {age}
-        }
-
+        print('Studenten är tillagd! \n')
+        
     elif menu_choice == '2': #måste komma åt alla värden i dictionaryn
-        print('Du valde att lista alla studenter: \n')
-        for värde in studenregister:
-            print(värde, ":", studenregister[värde])
-
-        #print('Du valde att lista alla studenter: \n' (studenregister))
+        print('Du valde att lista alla studenter: ')
+        print(studenregister)
 
     elif menu_choice == '3':
         print('Avslutar programmet...')
