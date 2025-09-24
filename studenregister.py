69d2b260ee57214bfad18a5265c4a722fedef5e5
Lisan <lisa.lassi@live.se>
2025-09-24 18:05:31 +0200

    loop problems

--- studenregister.py
+++ studenregister.py
@@ -1,14 +1,27 @@
 menu_is_running = True
 
 while menu_is_running:
-    menu_choice = input('Gör ett menyval \n[1] LÄGG TILL STUDENT \n[2] LISTA ALLA STUDENTER \n[3] AVSLUTA')
-
+    menu_choice = input('Gör ett menyval \n[1] LÄGG TILL STUDENT \n[2] LISTA ALLA STUDENTER \n[3] AVSLUTA \n Ditt val:')
+        
     if menu_choice == '1':
-        print()
+        print('Du valde att lägga till en student, ange förnamn & efternamn: ')
+        name = input('Förnamn: ')
+        lastname = input('Efternamn: ')
+        age = input('Ålder: ')
 
-    elif menu_choice == '2':
-        print()
+        studenregister ={
+        'Förnamn' : {name},
+        'Efternamn' : {lastname},
+        'Ålder' : {age}
+        }
+
+    elif menu_choice == '2': #måste komma åt alla värden i dictionaryn
+        print('Du valde att lista alla studenter: \n')
+        for värde in studenregister:
+            print(värde, ":", studenregister[värde])
+
+        #print('Du valde att lista alla studenter: \n' (studenregister))
 
     elif menu_choice == '3':
-        print()
+        print('Avslutar programmet...')
         break
