53ba922de38cbbb98b26a88422f99fd5e03859da
Lisan <lisa.lassi@live.se>
2025-09-24 17:39:46 +0200

    studenregister övningsuppgift

---
+++ studenregister.py
@@ -0,0 +1,14 @@
+menu_is_running = True
+
+while menu_is_running:
+    menu_choice = input('Gör ett menyval \n[1] LÄGG TILL STUDENT \n[2] LISTA ALLA STUDENTER \n[3] AVSLUTA')
+
+    if menu_choice == '1':
+        print()
+
+    elif menu_choice == '2':
+        print()
+
+    elif menu_choice == '3':
+        print()
+        break
